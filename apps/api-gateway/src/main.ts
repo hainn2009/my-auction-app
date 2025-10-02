0c3631b43b8cc62db7b3e771a7d05db5eb3b6f3e
Nguyen Ngoc Hai <hainn2009@gmail.com>
2025-10-02 14:53:23 +0700

    config port for gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { TrimStringsPipe } from './common/trim-strings.pipe';
@@ -6,15 +7,28 @@ const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('GATEWAY_PORT', 3000);
+  const frontendUrl = configService.get<string>('FRONTEND_URL', 'http://localhost:5174');
+
+  app.enableCors({
+    origin: frontendUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.use(cookieParser());
   app.useGlobalPipes(
     new TrimStringsPipe(),
     new ValidationPipe({
-      whitelist: true, // remove field does not exist in DTO definition
-      forbidNonWhitelisted: true, // throw error if field does not exist
+      whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
-  await app.listen(process.env.port ?? 3000);
+
+  await app.listen(port);
+  console.log(`🚀 API Gateway is running on: http://localhost:${port}`);
 }
 bootstrap();
