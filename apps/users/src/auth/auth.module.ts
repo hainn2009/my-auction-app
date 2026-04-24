fe8bccf0229f57bd0a040490f571a3fc0f429928
Nguyen Ngoc Hai <hainn2009@gmail.com>
2026-04-24 14:48:27 +0700

    fix user auth

--- apps/users/src/auth/auth.module.ts
+++ apps/users/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import type { SignOptions } from 'jsonwebtoken';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
@@ -15,7 +16,7 @@ import { PasswordService } from './password.service';
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET', 'defaultSecret'),
         signOptions: {
-          expiresIn: config.get<string>('JWT_EXPIRES_IN', '1h'),
+          expiresIn: config.get<string>('JWT_EXPIRES_IN', '1h') as SignOptions['expiresIn'],
         },
       }),
     }),
